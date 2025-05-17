eb571ac01b6de93af8bc061e31109800a31eb952
L3TIKBET <83845592+L3tikbet@users.noreply.github.com>
2025-05-16 21:03:25 -0300

    alterações no layout do chat

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const channelName = "teuNickEmMinusculo";
+const channelName = "user";
 
 const socket = new WebSocket("wss://irc-ws.chat.twitch.tv:443");
 
@@ -31,11 +31,36 @@ socket.onmessage = (event) => {
   }
 };
 
+const userColors = {};
+const colorPalette = [
+  "#FF69B4", "#00CED1", "#ADFF2F", "#FFA500", "#BA55D3", "#87CEEB", "#FF4500", "#40E0D0"
+];
+
+function getColorForUser(username) {
+  if (!userColors[username]) {
+    // Atribui uma cor da paleta, em loop
+    const colorIndex = Object.keys(userColors).length % colorPalette.length;
+    userColors[username] = colorPalette[colorIndex];
+  }
+  return userColors[username];
+}
+
+
 function displayMessage(user, text) {
   const chat = document.getElementById("chat-box");
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<span class="username">${user}:</span> ${text}`;
+
+  const usernameColor = getColorForUser(user);
+
+  div.innerHTML = `
+  <div class="username-box" style="background-color: ${usernameColor};">${user}</div>
+  <div class="message-text">${text}</div>
+`;
+
+
   chat.appendChild(div);
   chat.scrollTop = chat.scrollHeight;
 }
+
+
